6e8bd09affe832d3efbe9d9ea5c6c13475ee60d8
lusk <lucas78654321@outlook.com>
2022-09-17 13:19:09 -0300

    create const port

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express, { application } from "express";
 import { routes } from "./routes";
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 app.use(express.json());
 app.use(routes)
 
 
-app.listen(3000, () => console.log("Express server listening on port 3000"))
+app.listen(port, () => console.log("Express server listening on port 3000"))
