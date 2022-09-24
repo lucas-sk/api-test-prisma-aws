030b66f5d5f70b49f7371668dfda4c3ad41ea4e9
lusk <lucas78654321@outlook.com>
2022-09-24 10:19:16 -0300

    chore: add cors

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+
 import { prismaClient } from '../database/prismaClient';
 import { checkNamePass } from '../utils/checkNamePass';
 
@@ -7,12 +9,13 @@ export class CreateUserController {
   async handle(request: Request, response: Response) {
     try {
       const { name, password } = request.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
   
       if(checkNamePass(name, password)){
         return response.json({message: "name e password são obrigatórios"});
       };
   
-      const user = await prismaClient.user.create({ data: { name, password } });
+      const user = await prismaClient.user.create({ data: { name: name, password: hashedPassword } });
 
       return response.status(201).json(user);
     } catch(e){
