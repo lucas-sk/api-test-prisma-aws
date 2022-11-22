8672f8969b1b972e51da1fe93e203e34022ccaa2
lusk <lucas78654321@outlook.com>
2022-11-21 21:05:45 -0300

    push repo remote

--- src/controllers/FindUserController.ts
+++ src/controllers/FindUserController.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
 import { prismaClient } from '../database/prismaClient';
 
 export class FindUserController{
   async handle(request: Request, response: Response) {
     try {
-      
+
       const { id } = request.params
       const user = await prismaClient.user.findUnique({
           where: {
